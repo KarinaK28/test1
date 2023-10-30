9825514c739d4a5e3d2cf4cba7e2f3f97f594a9e
Karina <20hs.ak2831@gmail.com>
2023-10-30 07:35:24 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+
+'use strict';
+// let number = 5;
+// const leftBorderWidth = 1;
+ 
+// number = 10;
+// console.log(number);
+// alert('karina');
+
+// 'use strict';
+// const result = confirm('Are you here?');
+// console.log(result);
+// const answer = prompt('Are you 18?', '18');
+// console.log(answer + 5);
+const answers = [];
+answers[0] = prompt('NAme?', '');
+answers[1] = prompt('NachNAme?', '');
+answers[2] = prompt('Alt?', '');
+
+document.write(answers);
+
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+// const personalMovieDB = {
+// 	count: numberOfFilms,
+// 	movies: {},
+// 	actors: {},
+// 	genres: [],
+
+
+	
+// 	privat: false
+// };
+
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+// 	b = prompt('На сколько оцените его?', ''),
+// 	c = prompt('Один из последних просмотренных фильмов?', ''),
+// 	d = prompt('На сколько оцените его?', '');
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
